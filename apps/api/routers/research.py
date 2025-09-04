4fc955497971772ef756d178cc507d17853482c2
Macbook <fsaavedra003@gmail.com>
2025-09-04 22:53:57 +0200

    Initial commit

--- apps/api/routers/research.py
+++ apps/api/routers/research.py
@@ -3,3 +3,6 @@ from typing import Optional, List
 from fastapi import APIRouter, UploadFile, File, Form
 from pydantic import BaseModel
 from dotenv import load_dotenv
+
+
+from src.graph.agent_graph import build_graph
