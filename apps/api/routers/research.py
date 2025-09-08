575a12df097c9e23f5bb99800949c130aece0f4b
Macbook <fsaavedra003@gmail.com>
2025-09-08 22:48:37 +0200

    Initial commit

--- apps/api/routers/research.py
+++ apps/api/routers/research.py
@@ -42,7 +42,7 @@ async def ingest_endpoint(
     return IngestResponse(added_files=added, index_path=index_path)
 
 @router.post("/query")
-async def query_endpoint(req: ResearchRequest):
+async def query_endpoint
 
     
     
